a10e6a88cc837bce3a25fbc6fb0e042f5125b1de
Kiss <l3alazskiss15@gmail.com>
2022-11-22 10:30:30 -0400

    Added first page

--- Interactive Fiction - First Playable/Program.cs
+++ Interactive Fiction - First Playable/Program.cs
@@ -13,9 +13,12 @@ namespace Interactive_Fiction___First_Playable
         static void Main(string[] args)
         {
             StartGame();
-            PickPlot();
-            MakeYourChoice();
-            Console.ReadKey();
+            while (true)
+            {
+                PickPlot();
+                MakeYourChoice();
+                WaitForDecision();
+            }
         }
 
         static void StartGame()
@@ -40,6 +43,8 @@ namespace Interactive_Fiction___First_Playable
             Console.WriteLine("MAIN MENU");
             Console.WriteLine("_________");
             Console.WriteLine("A - New Game");
+            Console.WriteLine();
+            Console.Write("> ");
             var key = Console.ReadKey().Key;
             if (key == ConsoleKey.A)
             {
@@ -58,10 +63,7 @@ namespace Interactive_Fiction___First_Playable
             switch(pageNumber)
             {
                 case 1:
-                    Console.WriteLine("You live in the middle of a forest with your wife and two children." +
-                        " You built a house that kept your family warm in Winter, a well that supplied you with clean water" +
-                        " and a hunting tower to bring fresh meat." +
-                        " You're cooking the deer you hunted today when you see glowing eyes peering in the moonlight.");
+                    Console.WriteLine("You live in the middle of a forest with your wife and two children. You built a house that kept your family warm in Winter, a well that supplied you with clean water and a hunting tower to bring fresh meat. You're cooking the deer you hunted today when you see glowing eyes peering in the moonlight.");
                     break;
             }
         }
@@ -74,10 +76,32 @@ namespace Interactive_Fiction___First_Playable
             switch (pageNumber)
             {
                 case 1:
-                    Console.WriteLine("A - You take a few steps closer...");
-                    Console.WriteLine("B - You run into the house and lock the front door.");
+                    Console.WriteLine("A - You take a few steps closer... - Go to Page 2");
+                    Console.WriteLine("B - You run into the house and lock the front door. - Go to Page 3");
                     break;
             }
+        }
+
+        static void WaitForDecision()
+        {
+            Console.WriteLine();
+            Console.Write("> ");
+            switch (pageNumber)
+            {
+                case 1:
+                    var key = Console.ReadKey().Key;
+                    switch (key)
+                    {
+                        case ConsoleKey.A:
+                            pageNumber = 2;
+                            break;
+                        case ConsoleKey.B:
+                            pageNumber = 3;
+                            break;
+                    }
+                    break;
+            }
+            Console.WriteLine();
         }
     }
 }
