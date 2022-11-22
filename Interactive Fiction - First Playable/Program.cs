0f7ceedca40ec137db3739bb07534779f4e6cb73
Kiss <l3alazskiss15@gmail.com>
2022-11-22 14:53:55 -0400

    Added 6 endings

--- Interactive Fiction - First Playable/Program.cs
+++ Interactive Fiction - First Playable/Program.cs
@@ -23,9 +23,30 @@ namespace Interactive_Fiction___First_Playable
                     break;
                 }
                 WaitForDecision();
+                Console.Clear();
             }
             Console.WriteLine();
-            Console.WriteLine("You've reached the end, friend.");
+            switch (pageNumber)
+            {
+                case 3:
+                    Console.WriteLine("You've unlocked the safe ending.");
+                    break;
+                case 8:
+                    Console.WriteLine("You've unlocked the sad ending.");
+                    break;
+                case 6:
+                    Console.WriteLine("You've unlocked the happy ending.");
+                    break;
+                case 4:
+                    Console.WriteLine("You've unlocked the rough ending.");
+                    break;
+                case 11:
+                    Console.WriteLine("You've unlocked the curious ending.");
+                    break;
+                case 10:
+                    Console.WriteLine("You've unlocked the tiresome ending.");
+                    break;
+            }
             Console.WriteLine();
             Console.ReadKey();
         }
@@ -34,6 +55,10 @@ namespace Interactive_Fiction___First_Playable
         {
             Console.WriteLine("ADJUST CONSOLE SCREEN SIZE");
             Console.WriteLine(". . .then press any key to begin. . .");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("There are 6 possible endings.");
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine();
             Console.ReadKey();
             Console.Clear();
@@ -72,13 +97,37 @@ namespace Interactive_Fiction___First_Playable
             switch(pageNumber)
             {
                 case 1:
-                    Console.WriteLine("You live in the middle of a forest with your wife and two children. You built a house that kept your family warm in Winter, a well that supplied you with clean water and a hunting tower to bring fresh meat. You're cooking the deer you hunted today when you see glowing eyes peering in the moonlight.");
+                    Console.WriteLine("You live in the middle of a forest with your wife and two children. You built a house that keeps your family warm in Winter, a well that supplied you with clean water and a hunting tower to bring fresh meat. You're cooking the deer you hunted today when you see glowing eyes peering in the moonlight.");
                     break;
                 case 2:
                     Console.WriteLine("You tiptoe. The eyes vanish and you see a glimpse of the body.");
                     break;
                 case 3:
                     Console.WriteLine("You dash left into the kitchen and look out the window. You see it now, it's a wolf.");
+                    break;
+                case 5:
+                    Console.WriteLine("You take your lantern. The figure runs into a cave, you're starting to realize this is dangerous.");
+                    break;
+                case 7:
+                    Console.WriteLine("You forgot where your home is. You start to worry.");
+                    break;
+                case 8:
+                    Console.WriteLine("You're becoming restless and hungry. You hear howling. That's not good. You see the same eyes staring at you. Multiple.");
+                    break;
+                case 6:
+                    Console.WriteLine("The next morning comes, the birds are singing, the sun is shining, your children are calling your name. They found you! You're crying with joy.");
+                    break;
+                case 4:
+                    Console.WriteLine("You're almost back home. You're a bit angry and trip over a twig. Your clothes are dirty. It has been a long day but you're happy now that you're with your family.");
+                    break;
+                case 9:
+                    Console.WriteLine("The cave is dark. The eyes stop and turn toward you. You extend your arm for a better view. It's a wolf.");
+                    break;
+                case 11:
+                    Console.WriteLine("Without showing fear, the wolf runs away into the cave. You get back home and you're greeted with dinner. Your family is relieved that you're back and you finally get some sleep.");
+                    break;
+                case 10:
+                    Console.WriteLine("The wolf chases you. Luckily, there is a big rock that will give you high ground. You climb it and the wolf runs away. You make your way back home.");
                     break;
             }
         }
@@ -91,18 +140,57 @@ namespace Interactive_Fiction___First_Playable
                     Console.WriteLine();
                     Console.WriteLine("MAKE YOUR CHOICE:");
                     Console.WriteLine();
-                    Console.WriteLine("A - You take a few steps closer... - Go to Page 2");
-                    Console.WriteLine("B - You run into the house and lock the front door. - Go to Page 3");
+                    Console.WriteLine("A - Take a few steps closer... - Go to Page 2");
+                    Console.WriteLine("B - Run into the house and lock the front door. - Go to Page 3");
                     break;
                 case 2:
                     Console.WriteLine();
                     Console.WriteLine("MAKE YOUR CHOICE:");
                     Console.WriteLine();
-                    Console.WriteLine("A - You chase the figure. - Go to Page 10");
-                    Console.WriteLine("B - You pretend like you never saw anything. - Go to Page 27");
+                    Console.WriteLine("A - Chase the figure. - Go to Page 5");
+                    Console.WriteLine("B - Pretend like you never saw anything. - Go to Page 4");
                     break;
                 case 3:
+                    Console.WriteLine();
                     Console.WriteLine("You reassure your family that everything is alright and you all go to bed feeling safe from the dangers of the outside world.");
+                    stillReadingBook = false;
+                    break;
+                case 5:
+                    Console.WriteLine();
+                    Console.WriteLine("MAKE YOUR CHOICE:");
+                    Console.WriteLine();
+                    Console.WriteLine("A - Go into the cave. - Go to Page 9");
+                    Console.WriteLine("B - Head back. - Go to Page 7");
+                    break;
+                case 7:
+                    Console.WriteLine();
+                    Console.WriteLine("MAKE YOUR CHOICE:");
+                    Console.WriteLine();
+                    Console.WriteLine("A - Climb a tree to sleep in. - Go to Page 6");
+                    Console.WriteLine("B - Continue searching. - Go to Page 8");
+                    break;
+                case 8:
+                    stillReadingBook = false;
+                    break;
+                case 6:
+                    Console.WriteLine();
+                    Console.WriteLine("You come back home to a warm vegetable stew. You play ball with your children and you dance with your wife. It has been a wonderful day.");
+                    stillReadingBook = false;
+                    break;
+                case 4:
+                    stillReadingBook = false;
+                    break;
+                case 9:
+                    Console.WriteLine();
+                    Console.WriteLine("MAKE YOUR CHOICE:");
+                    Console.WriteLine();
+                    Console.WriteLine("A - Freeze, then stare at the wolf. - Go to Page 11");
+                    Console.WriteLine("B - Run away. - Go to Page 10");
+                    break;
+                case 11:
+                    stillReadingBook = false;
+                    break;
+                case 10:
                     stillReadingBook = false;
                     break;
             }
@@ -131,10 +219,46 @@ namespace Interactive_Fiction___First_Playable
                     switch (key2)
                     {
                         case ConsoleKey.A:
-                            pageNumber = 10;
+                            pageNumber = 5;
                             break;
                         case ConsoleKey.B:
-                            pageNumber = 27;
+                            pageNumber = 4;
+                            break;
+                    }
+                    break;
+                case 5:
+                    var key3 = Console.ReadKey().Key;
+                    switch (key3)
+                    {
+                        case ConsoleKey.A:
+                            pageNumber = 9;
+                            break;
+                        case ConsoleKey.B:
+                            pageNumber = 7;
+                            break;
+                    }
+                    break;
+                case 7:
+                    var key4 = Console.ReadKey().Key;
+                    switch (key4)
+                    {
+                        case ConsoleKey.A:
+                            pageNumber = 6;
+                            break;
+                        case ConsoleKey.B:
+                            pageNumber = 8;
+                            break;
+                    }
+                    break;
+                case 9:
+                    var key5 = Console.ReadKey().Key;
+                    switch (key5)
+                    {
+                        case ConsoleKey.A:
+                            pageNumber = 11;
+                            break;
+                        case ConsoleKey.B:
+                            pageNumber = 10;
                             break;
                     }
                     break;
